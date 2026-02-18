3b195aaf3c71bad0f4c4bcec58026482260007a2
DanielMamytov <mamytovdaniel.08.02@gmail.com>
2026-02-18 18:39:49 +0600

    .

--- app/src/main/java/com/example/feedup/data/network/CharacterApi.kt
+++ app/src/main/java/com/example/feedup/data/network/CharacterApi.kt
@@ -3,8 +3,12 @@ package com.example.feedup.data.network
 import com.example.feedup.model.TaskItem
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface CharacterApi {
 
@@ -13,4 +17,14 @@ interface CharacterApi {
 
     @POST("task")
     fun createTask(@Body task: TaskItem): Call<TaskItem>
+
+    @PUT("task/{id}")
+    fun updateTask(@Path("id") id: String, @Body task: TaskItem): Call<TaskItem>
+
+
+    @PATCH("task/{id}")
+    fun patchTask(@Path("id") id: String, task: Map<String, String>): Call<TaskItem>
+
+    @DELETE("task/{id}")
+    fun deleteTask(@Path("id") id: String): Call<Unit>
 }
